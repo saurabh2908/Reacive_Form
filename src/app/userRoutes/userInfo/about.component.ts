0c5e19b1f048b33fdccc0058f9ababc76543bc9e
Saurabh singh <2908aman@gmail.com>
2019-09-08 12:11:41 +0530

    Reactive- formvaldation

--- src/app/userRoutes/userInfo/about.component.ts
+++ src/app/userRoutes/userInfo/about.component.ts
@@ -10,7 +10,7 @@ import { Component, OnInit } from '@angular/core';
 export class AboutComponent implements OnInit {
   
   regiterForm:FormGroup;
-  blackListedUsers:string[] = ["Mayank harbola","kim","rim","sim"];
+  blackListedUsers:string[] = ["Mayank harbola","Naresh bisht","Abhishek sindhvani"];
 
   ngOnInit(){
       this.regiterForm = new FormGroup({
