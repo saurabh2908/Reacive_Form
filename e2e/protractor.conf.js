9abb3b88e4ea14f378a6204ecc6b73e17ed65e40
Saurabh singh <2908aman@gmail.com>
2019-09-09 00:16:50 +0530

    Angular-Routing with ReactiveForm validations

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -1,7 +1,7 @@
 // @ts-check
 // Protractor configuration file, see link for more information
 // https://github.com/angular/protractor/blob/master/lib/config.ts
-
+// This is e2e
 const { SpecReporter } = require('jasmine-spec-reporter');
 
 /**
